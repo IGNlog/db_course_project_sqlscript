8abd62efb4f6bbd5f93f0f6af0af2f5ce87e91d8
Ildar Gibadullin <evlampiieb@gmail.com>
2017-10-01 02:12:02 +0300

    Add select 1 and select 4

---
+++ SELECT_1.sql
@@ -0,0 +1,18 @@
+USE [CP_BASE]
+GO
+CREATE PROCEDURE [dbo].[Get_All_Amount_N](@N REAL)
+AS
+	BEGIN
+		SET NOCOUNT ON;
+		SELECT [WAREHOUSE_NUMBER] AS 'Номер склада', [PROD_CODE] AS 'Код товара', [ALL_AMOUNT] AS 'Общий объём'
+		FROM [dbo].[STOR_PROD]
+		WHERE ALL_AMOUNT>@N;
+	END
+GO
+
+USE CP_BASE
+GO
+DECLARE @N REAL
+SET @N=20;
+EXECUTE Get_All_Amount_N @N;
+GO
